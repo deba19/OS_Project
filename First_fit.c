ac709f2753edc7e8a6b855e80269f6c4eb45d32f
Debasis Jana <debasisjana93@gmail.com>
2019-04-13 21:32:10 +0530

    Update First_fit.c
    Added BestFit Algorithm

--- First_fit.c
+++ First_fit.c
@@ -5,7 +5,7 @@ int firstFit(int blockSize[], int m,
 { 
 	int i,j;
 	int allocation[n];  
-	memset(allocation, -1, sizeof(allocation)); 
+	//memset(allocation, -1, sizeof(allocation)); 
 	for (i = 0; i < n; i++) 
 	{ 
 		for (j = 0; j < m; j++) 
@@ -30,6 +30,40 @@ int firstFit(int blockSize[], int m,
 			printf("Not Allocated\n"); 
 	} 
 } 
+
+int bestFit(int blockSize[], int m, int processSize[], int n) 
+{ 
+    int allocation1[n],i,j;  
+    for (i=0; i<n; i++) 
+    { 
+        int bestIdx = -1; 
+        for ( j=0; j<m; j++) 
+        { 
+            if (blockSize[j] >= processSize[i]) 
+            { 
+                if (bestIdx == -1) 
+                    bestIdx = j; 
+                else if (blockSize[bestIdx] > blockSize[j]) 
+                    bestIdx = j; 
+            } 
+        } 
+        if (bestIdx != -1) 
+        { 
+            allocation1[i] = bestIdx;  
+            blockSize[bestIdx] -= processSize[i]; 
+        } 
+    } 
+  
+    printf("\nProcess No.\tProcess Size\tBlock no.\n"); 
+    for ( i = 0; i < n; i++) 
+    { 
+        printf("   %d\t\t%d\t\t",i+1,processSize[i]); 
+        if (allocation1[i] != -1) 
+            printf("%d\n",allocation1[i] + 1); 
+        else
+            printf("Not Allocated\n");  
+    } 
+} 
 int main() 
 { 
  	int n,m;
@@ -44,8 +78,24 @@ int main()
 	printf("Enter Process Partiations:\n");
 	for(i=0;i<n;i++)
 	scanf("%d",&processSize[i]);
+	system("cls");
+	printf("1.FirstFit\n2.BestFit\n3.WorstFit\n\n");
+	int choice;
+	printf("Enter which algo you want choose\n");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+			firstFit(blockSize, m, processSize, n);
+			break;
+		case 2:	 
+			bestFit(blockSize, m, processSize, n);
+			break;
+		case 3:
+			break;
+		default:
+			printf("Wrong Choice");
+	}
 
-	firstFit(blockSize, m, processSize, n); 
 	return 0 ; 
 } 
-
